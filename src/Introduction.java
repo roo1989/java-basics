f51ac7de87dd79eefb804f85779535e17d51273c
Ragnar Olafsson <ragnar.orn@gmail.com>
2021-09-01 12:20:37 +0200

    Make the firstName variable more dynamic by reading user input

--- src/Introduction.java
+++ src/Introduction.java
@@ -3,7 +3,12 @@ import java.io.Console;
 public class Introduction {
     public static void main(String[] args) {
         Console console = System.console();
-        String firstName = "Selma";
+        // String firstName = "Selma";
+
+        // Let's make the firstName variable a bit more dynamic by reading from the keyboard.
+
+        String firstName = console.readLine("What is your name? ");
+
 
         console.printf("Hello, my name is %s\n", firstName);
         //console.printf("Hello, my name is Ragnar\n");
