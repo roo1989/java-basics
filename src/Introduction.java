5d495435a12605149d64ba745f6227438d4909ee
Ragnar Olafsson <ragnar.orn@gmail.com>
2021-09-01 11:13:12 +0200

    Initial hello world commit

---
+++ src/Introduction.java
@@ -0,0 +1,8 @@
+import java.io.Console;
+
+public class Introduction {
+    public static void main(String[] args) {
+        Console console = System.console();
+        console.printf("Hello, my name is Ragnar");
+    }
+}
