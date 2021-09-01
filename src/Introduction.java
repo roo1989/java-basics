6fa0343ea11865d63c00bbac28bd941568963dd4
Ragnar Olafsson <ragnar.orn@gmail.com>
2021-09-01 12:12:11 +0200

    Add sequence character for strings and learn how to declare variables

--- src/Introduction.java
+++ src/Introduction.java
@@ -3,6 +3,11 @@ import java.io.Console;
 public class Introduction {
     public static void main(String[] args) {
         Console console = System.console();
-        console.printf("Hello, my name is Ragnar");
+        String firstName = "Selma";
+
+        console.printf("Hello, my name is %s\n", firstName);
+        //console.printf("Hello, my name is Ragnar\n");
+        // The \n is an called an escape character. This one handles newlines after the string.
+        console.printf("%s is learning how to write Java\n", firstName);
     }
 }
