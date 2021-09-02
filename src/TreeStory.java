1893b6c67cc997ce357dea5b1a0e7bed70574230
Ragnar Olafsson <ragnar.orn@gmail.com>
2021-09-02 11:03:25 +0200

    Fix dangling javadoc comment.

--- src/TreeStory.java
+++ src/TreeStory.java
@@ -1,15 +1,15 @@
 import java.io.Console;
 
 public class TreeStory {
+    /**
+     * Some terms:
+     * noun - Person, place or thing
+     * verb - An action
+     * adjective - A description used to modify or describe a noun
+     * Enter your amazing code here!
+     */
     public static void main(String[] args) {
         Console console = System.console();
-        /**
-         * Some terms:
-         * noun - Person, place or thing
-         * verb - An action
-         * adjective - A description used to modify or describe a noun
-         * Enter your amazing code here!
-         */
         String name = console.readLine("Enter your name:  ");
         String adjective = console.readLine("Enter an adjective:  ");
         String noun = console.readLine("Enter a noun:  ");
