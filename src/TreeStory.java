693467713ffc4c061943784dcc75e5321a084084
Ragnar Olafsson <ragnar.orn@gmail.com>
2021-09-03 11:13:31 +0200

    Look at String methods - add the String method equalsIgnoreCase() to check for censored words.

--- src/TreeStory.java
+++ src/TreeStory.java
@@ -21,6 +21,10 @@ public class TreeStory {
         String name = console.readLine("Enter your name:  ");
         String adjective = console.readLine("Enter an adjective:  ");
         String noun = console.readLine("Enter a noun:  ");
+        if (noun.equalsIgnoreCase("dork")) {
+            console.printf("That language is not allowed! Exiting \n\n");
+            System.exit(0);
+        }
         String adverb = console.readLine("Enter an adverb: ");
         String verb = console.readLine("Enter a verb ending with -ing:  ");
 
