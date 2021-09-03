422e1d24c02f4dc181207012c51f19166ebf4807
Ragnar Olafsson <ragnar.orn@gmail.com>
2021-09-03 11:01:41 +0200

    Make the age dynamic by using the wrapper class Integer and the static method of that class parseInt() to convert a String data type to a integer.

--- src/TreeStory.java
+++ src/TreeStory.java
@@ -10,7 +10,9 @@ public class TreeStory {
      */
     public static void main(String[] args) {
         Console console = System.console();
-        int age = 12;
+        String ageAsString = console.readLine("How old are you?  ");
+
+        int age = Integer.parseInt(ageAsString);
         if (age < 13) {
             // Insert exit code.
             console.printf("Sorry, you must be at least 13 to use this program \n");
